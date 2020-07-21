1adaa398a82b7b6e1d06ac7cdb7ed98ebf33c5b6
shriram8898 <51223184+shriram8898@users.noreply.github.com>
2020-07-21 10:59:26 +0530

    Updated

---
+++ TaskApp/Models/comments.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskApp.Models
+{
+    public class comments
+    {
+        public string id { get; set; }
+        public string message { get; set; }
+        public string empid { get; set; }
+        public string empname { get; set; }
+        public DateTime dt { get; set; }
+    }
+}
