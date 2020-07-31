108b83d59b2511d45881e5d776d13f2c2833c0b6
shriram8898 <51223184+shriram8898@users.noreply.github.com>
2020-07-31 11:43:49 +0530

    Update

---
+++ TaskApp(1)/Models/Audio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskApp.Models
+{
+    public class Audio
+    {
+        public string taskid { get; set; }
+        public string file { get; set; }
+    }
+}
---
+++ TaskApp(1)/Models/IRecordAudio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskApp.Models
+{
+    public interface IRecordAudio
+    {
+        Task CreateAudioGraph();
+        Task PlayAudioFile(string fileName);
+        void RecordOrStop();
+        void OpenAudioFilePath();
+
+    }
+}
